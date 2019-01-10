7118cacbc66a7af689cd2b3005bdfe4dc3824460
rookie <417095066@qq.com>
2019-01-10 16:39:45 +0800

    add Solution

--- Solution/2.Add Two Numbers.js
+++ Solution/2.Add Two Numbers.js
@@ -12,4 +12,27 @@
  */
 var addTwoNumbers = function(l1, l2) {
     var c1 = l1,c2 = l2,c3,l3,carry = 0;
+    var c = 0;
+    var ret = new ListNode(0);
+    var curr = ret;
+
+    while (l1 || l2) {
+        var a = l1 ? l1.val : 0;
+        var b = l2 ? l2.val : 0;
+        var sum = a + b + c;
+        c = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+        if (l1) {
+            l1 = l1.next;
+        }
+        if (l2) {
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+      if (c) {
+        curr.next = new ListNode(c);
+    }
+
+      return ret.next;
 };
