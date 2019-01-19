7b7de62e74332c2638a8c4b16a182472f77af58a
rookie <417095066@qq.com>
2019-01-19 23:16:00 +0800

    add Solution

---
+++ Solution/33.Search in Rotated Sorted Array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function(nums, target) {
+    for(var i = 0, len = nums.length; i < len; i++)
+      if (nums[i] === target)
+        return i;
+    return -1;
+  };
