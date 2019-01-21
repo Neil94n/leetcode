243cccd2332c33735d08054a435f8cc7f3375c6a
rookie <417095066@qq.com>
2019-01-21 22:57:18 +0800

    add Solution

---
+++ Solution/55.Jump Game.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var canJump = function(nums) {
+    var rightMost = 1;
+    for (var i = 0, len = nums.length; i < len; i++) {
+      if (rightMost < i + 1) break;
+      rightMost = Math.max(rightMost, i + 1 + nums[i]);
+    }
+    return rightMost >= len;
+  };
