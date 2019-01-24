da0aca6c0991a5febec7f220619ebd5feae2402f
rookie <417095066@qq.com>
2019-01-24 19:32:33 +0800

    add Solution

---
+++ Solution/69.Sqrt(x).js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+// I really do not know what the meaning of this problem is
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x));
+  };
