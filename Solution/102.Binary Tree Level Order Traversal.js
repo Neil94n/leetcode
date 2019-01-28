6f88ac24136800bf3be28c5e4cb1577e3a5b953e
rookie <417095066@qq.com>
2019-01-28 17:42:17 +0800

    add Solution

---
+++ Solution/102.Binary Tree Level Order Traversal.js
@@ -0,0 +1,28 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return [];
+  
+    var ans = []
+      , tmp = [root];
+  
+    while (tmp.length) {
+      var res = []
+        , a = [];
+  
+      for (var i = 0, len = tmp.length; i < len; i++) {
+        if (!tmp[i]) continue;
+        res.push(tmp[i].val);
+        a.push(tmp[i].left);
+        a.push(tmp[i].right);
+      }
+  
+      tmp = a.concat();
+      if (res.length)
+        ans.push(res);
+    }
+  
+    return ans;
+  };
