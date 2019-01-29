0b68f700adbae4fb6bd77450e6783f4dfcb049fd
rookie <417095066@qq.com>
2019-01-29 23:36:15 +0800

    add Solution

---
+++ Solution/110.Balanced Binary Tree.js
@@ -0,0 +1,25 @@
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var ans;
+
+function dfs(root) {
+  if (!root) return;
+  
+  var a = root.left ? dfs(root.left) : 0
+    , b = root.right ? dfs(root.right) : 0;
+
+  if (Math.abs(a - b) > 1) {
+    ans = false;
+  }
+
+  return Math.max(a, b) + 1;
+}
+
+var isBalanced = function(root) {
+  ans = true;
+  dfs(root);
+  return ans;
+};
