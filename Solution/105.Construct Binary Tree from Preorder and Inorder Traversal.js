4f2bb24c78e5e8f2bf5bdf119dbbba787d1ac92a
rookie <417095066@qq.com>
2019-01-29 23:30:32 +0800

    add Solution

---
+++ Solution/105.Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    return dfs(0, 0, preorder.length - 1);
+  
+    // the subTree values whose node value is preorder[index]
+    // inorder[startPos] - inorder[endPos]
+    function dfs(index, startPos, endPos) {
+      if (startPos > endPos)
+        return null;
+  
+      var node = new TreeNode(preorder[index]);
+      var pos = inorder.indexOf(preorder[index], startPos);
+  
+      // node's left subNode's value is preorder[index + 1]
+      // node's right subNode's value is preorder[index + pos - startPos + 1]
+      node.left = dfs(index + 1, startPos, pos - 1);
+      node.right = dfs(index + pos - startPos + 1, pos + 1, endPos);
+  
+      return node;
+    }
+  };
