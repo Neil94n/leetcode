f1af6e5f078e224340dd69dc309c25928da0d3e3
rookie <417095066@qq.com>
2019-02-02 19:38:50 +0800

    add Solution

---
+++ Solution/125.Valid Palindrome.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ 
+var isPalindrome = function(s) {
+    s = s.replace(/[\W]/g, '');
+    s = s.toLowerCase();
+    var _s = s.split('').reverse().join('');
+    return s === _s;
+  };
