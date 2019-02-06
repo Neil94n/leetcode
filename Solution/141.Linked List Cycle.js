e146d89eb551534106d900812a493b9673aee603
rookie <417095066@qq.com>
2019-02-06 21:11:52 +0800

    add Solution

---
+++ Solution/141.Linked List Cycle.js
@@ -0,0 +1,31 @@
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    flag = false;
+  
+    dfs(head);
+  
+    return flag;
+  };
+  
+  function dfs(node) {
+    if (flag)
+      return;
+  
+    if (node === null)
+      return;
+  
+    if (node.flag) {
+      flag = true;
+      return;
+    }
+  
+    if (node.next === null)
+      return;
+  
+    node.flag = true;
+  
+    dfs(node.next);
+  }
