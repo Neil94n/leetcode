eee05f4c98400ec1b87c8df51a68d060e848a8fa
rookie <417095066@qq.com>
2019-02-08 19:29:49 +0800

    add Solution

---
+++ Solution/151.Reverse Words in a String.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+    str = str.trim();
+    str = str.replace(/\s+/g, ' ');
+    var arr = str.split(' ').reverse();
+    return arr.join(' ');
+  };
