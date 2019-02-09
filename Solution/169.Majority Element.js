094cab92ba38cfa8e415419e337af2d061651706
rookie <417095066@qq.com>
2019-02-09 21:44:31 +0800

    add Solution

---
+++ Solution/169.Majority Element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var majorityElement = function(nums) {
+    var hash = [];
+    for(var i = 0, len = nums.length; i < len; i++) {
+      if (!hash[nums[i]]) hash[nums[i]] = 1;
+      else hash[nums[i]]++;
+  
+      if (hash[nums[i]] > len / 2)
+        return nums[i];
+    }
+  };
+  
