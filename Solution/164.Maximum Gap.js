6b3409399aad68bfc1e528e86a890833a6d8b960
rookie <417095066@qq.com>
2019-02-09 21:40:03 +0800

    add Solution

---
+++ Solution/164.Maximum Gap.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+    if (nums.length < 2)
+      return 0;
+  
+    nums.sort(function(a, b) {
+      return a - b;
+    });
+  
+    var ans = -1;
+    nums.reduce(function(pre, cur, index, array) {
+      ans = Math.max(ans, cur - pre);
+      return cur;
+    });
+  
+    return ans;
+  };
