7c79254a8e3dc10297d95bab69777879e160b4c6
rookie <417095066@qq.com>
2019-02-10 12:14:51 +0800

    add Solution

---
+++ Solution/189.Rotate Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var rotate = function(nums, k) {
+    k %= nums.length;
+    var tmp = [];
+    if (k)
+      tmp = nums.slice(-k);
+    nums.splice(-k, k);
+  
+    Array.prototype.unshift.apply(nums, tmp);
+  };
